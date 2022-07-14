aee317a08c85a2fc23c5d56dbb4bdacfae03de93
lifeng <lingyan_x@126.com>
2022-07-14 22:45:32 +0800

    init module

---
+++ springboot-zookeeper/src/main/java/com/codex/springbootzookeeper/SpringbootZookeeperApplication.java
@@ -0,0 +1,16 @@
+package com.codex.springbootzookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lifeng
+ */
+@SpringBootApplication
+public class SpringbootZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootZookeeperApplication.class, args);
+    }
+
+}
---
+++ springboot-zookeeper/src/test/java/com/codex/springbootzookeeper/SpringbootZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codex.springbootzookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
